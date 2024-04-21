c5b7684144d440e985f5259983ba03414c4cbcbb
Rishav <111266613+rissshaaav@users.noreply.github.com>
2024-04-21 22:30:25 +0530

    initalized a basic react app and intalled tailwind in it

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen min-w-screen bg-slate-300 flex justify-center items-center h-screen">
+      <div className="w-[95%] h-[90%] bg-white rounded-lg">
+        <h1 className="text-4xl font-bold text-gray-800">Hello World</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
